123a667bdb9c290315aea7ea51ac87aa4b71a621
whatgum <whatgum@gmail.com>
2020-10-03 17:45:24 -0400

    did more to make the text come in bettter

--- Assets/dialoguePlacer.cs
+++ Assets/dialoguePlacer.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.UIElements;
 
 public class dialoguePlacer : MonoBehaviour
@@ -10,7 +11,8 @@ public class dialoguePlacer : MonoBehaviour
     private char[] textToDisplay;
     private TMPro.TextMeshPro textDisplay;
     private int lengthOfText;
-    
+    private Text text;
+    private float fadeInTime = .04f;
 
     public void Initalize(string text)
     {
@@ -24,17 +26,32 @@ public class dialoguePlacer : MonoBehaviour
     void Start()
     {
         textDisplay = GetComponent<TMPro.TextMeshPro>();
-
+        text = GetComponent<Text>();
     }
 
-
+    private IEnumerator FadeCharacter(char character)
+    {
+        Color originalColor = text.color;
+        string grexText = "<color = clear>" + character + "</color>";
+        string currentText = text.text;
+        text.text = text.text + grexText;
+        for (float t = 0.01f; t < fadeInTime; t += Time.deltaTime)
+        {
+            text.text = currentText;
+            Color color = Color.Lerp(originalColor, Color.clear, Mathf.Min(1, t / fadeInTime));
+            grexText = "<color = "+ ColorUtility.ToHtmlStringRGBA(color) + ">" + character + "</color>";
+            text.text = text.text + grexText;
+            yield return null;
+        }
+    }
 
     private IEnumerator DisplayText()
     {
         for(int i = 0; i < lengthOfText; i++)
         {
-            yield return new WaitForSeconds(.05f);
-            textDisplay.text = textDisplay.text + textToDisplay[i];
+            yield return new WaitForSeconds(.01f);
+            StartCoroutine(FadeCharacter(textToDisplay[i]));
+            
 
         }
     }
